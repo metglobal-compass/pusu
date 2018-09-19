17fa65685e925918d6f43be1173e7cac3648be8f
Seyfi Aslan <seyfiaslangecinen@gmail.com>
2018-09-19 23:18:55 +0300

    Added app engine runner

---
+++ adapters/google/appenginerunner.go
@@ -0,0 +1,15 @@
+package google
+
+import (
+	"github.com/metglobal-compass/pusu"
+	"google.golang.org/appengine"
+)
+
+// App engine http handler which implements pusu.Runner interface
+type appEngineRunner struct {
+}
+
+func (a *appEngineRunner) Run(subscription *pusu.Subscription) error {
+	appengine.Main()
+	return nil
+}
